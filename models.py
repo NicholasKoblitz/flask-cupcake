03c895e9c2ff4cf2018139e18398031c9f863110
NicholasK <nicholask1@outlook.com>
2022-01-29 14:08:28 -0500

    Created model for cupcakes table

---
+++ models.py
@@ -0,0 +1,33 @@
+"""Models for Cupcake app."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    """Connects to database"""
+
+    db.app = app
+    db.init_app(app)
+
+
+class Cupcake(db.Model):
+
+    __tablename__ = "cupcakes"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    flavor = db.Column(db.Text, nullable=False)
+    size = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    image = db.Column(db.Text, nullable=False, default=" https://tinyurl.com/demo-cupcake")
+
+
+def serialize_cupcake(cupcake):
+
+        return {
+            "id": cupcake.id,
+            "flavor": cupcake.flavor,
+            "size": cupcake.size,
+            "rating": cupcake.rating,
+            "image": cupcake.image
+        }  
